005abce0c2311d6eb9921df1078b79e9241dba93
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-05-09 20:31:18 +0200

    Modifiche grafiche

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,38 @@ import React from 'react'
 
 import { Link } from 'react-router-dom';
 
+import {AiOutlineHome} from 'react-icons/ai';
+import {TbListSearch} from 'react-icons/tb';
+import {MdOutlineDashboardCustomize} from 'react-icons/md';
+
 const PATH = window.location.pathname.split(`/`)[1];
 
 const Navbar = () => {
   return (
     <header className='header-navbar'>
       <nav>
-        <Link to={`${PATH}/`}>Home</Link>
-        <Link to={`${PATH}/search`}>Cerca</Link>
-        <Link to={`${PATH}/add`}>Aggiungi</Link>
+
+        <Link to={`${PATH}/`}>
+          <span className="nav-icons">
+            <AiOutlineHome/>
+            <span className='icons-text'>Home</span>
+          </span>
+        </Link>
+
+        <Link to={`${PATH}/search`}>
+          <span className="nav-icons">
+            <TbListSearch/>
+            <span className='icons-text'>Cerca</span>
+          </span>
+        </Link>
+
+        <Link to={`${PATH}/add`}>
+          <span className="nav-icons">
+            <MdOutlineDashboardCustomize/>
+            <span className='icons-text'>Personalizza</span>
+          </span>
+        </Link>
+
       </nav>
     </header>
   )
