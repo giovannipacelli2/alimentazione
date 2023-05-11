a9a9ffa05631ad021cd6f6d0dd847aab2b4fb466
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-05-11 22:40:15 +0200

    links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,28 +13,28 @@ const Navbar = () => {
     <header className='header-navbar'>
       <nav>
 
-        <Link to={`${PATH}/`}>
+        <Link to={`${PATH}`}>
           <span className="nav-icons">
             <AiOutlineHome/>
             <span className='icons-text'>Home</span>
           </span>
         </Link>
 
-        <Link to={`${PATH}/summary`}>
+        <Link to={`${PATH}summary`}>
           <span className="nav-icons">
             <MdOutlineDataset/>
             <span className='icons-text'>Riepilogo</span>
           </span>
         </Link>
 
-        <Link to={`${PATH}/search`}>
+        <Link to={`${PATH}search`}>
           <span className="nav-icons">
             <TbListSearch/>
             <span className='icons-text'>Cerca</span>
           </span>
         </Link>
 
-        <Link to={`${PATH}/add`}>
+        <Link to={`${PATH}add`}>
           <span className="nav-icons">
             <MdOutlineDashboardCustomize/>
             <span className='icons-text'>Modifica</span>
