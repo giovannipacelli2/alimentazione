7f73bb2f6ea1478ae477ff697bc83d3f9c2d6552
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-05-13 13:00:25 +0200

    Cambio stile tabella riepilogo

--- src/components/SumFood.js
+++ src/components/SumFood.js
@@ -12,10 +12,18 @@ const SumFood = ({total, setCopyTotal}) => {
                     <h3>Totale kcal: <span className='macro-color'>{total.kcal.toFixed(1)}</span></h3>
                 </div>
 
-                <div className='food-description'>                    
-                    <h4>carbo: <span className='macro-color'>{total.carbo.toFixed(1)}</span></h4>
-                    <h4>prot: <span className='macro-color'>{total.prot.toFixed(1)}</span></h4>
-                    <h4>fat: <span className='macro-color'>{total.fat.toFixed(1)}</span></h4>
+                <div className='food-description'> 
+                    <div className="macro">
+                        <h4>carbo: <span className='macro-color'>{total.carbo.toFixed(1)}</span></h4>
+                    </div>    
+
+                    <div className="macro">
+                        <h4>prot: <span className='macro-color'>{total.prot.toFixed(1)}</span></h4>
+                    </div>    
+                                   
+                    <div className="macro">
+                        <h4>fat: <span className='macro-color'>{total.fat.toFixed(1)}</span></h4>
+                    </div>                   
                 </div>
 
                 <button 
